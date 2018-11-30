45ae663aeed68ac4f6063df35fa8983f64cc484c
AttreaVe <johansiautama@gmail.com>
2018-11-30 18:12:57 +0700

    on press console log

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,44 @@
 // @flow
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import Layout from '../general/layouts/index';
+
+let menuSection = (name) => (
+  <TouchableOpacity
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth: 1,
+    }}
+    onPress={() => {
+      console.log(name);
+    }}
+  >
+    <Text style={{fontSize: 20}}>{name}</Text>
+  </TouchableOpacity>
+);
 
 const IndexPage = () => (
   <Layout title={'Home'}>
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, justifyContent: 'flex-start'}}>
       <Text style={{fontSize: 20}}>Ha</Text>
     </View>
     <View
       style={{
         flexDirection: 'row',
         width: '100%',
-        height: 100,
         alignItems: 'center',
         justifyContent: 'center',
         // height: '100%',
         backgroundColor: 'blue',
       }}
     >
-      <View style={{flex: 1}}>
-        <Text style={{fontSize: 20}}>Ha</Text>
-      </View>
-      <View style={{flex: 1}}>
-        <Text style={{fontSize: 20}}>Ha</Text>
-      </View>
+      {menuSection('Home')}
+      {menuSection('Simulation')}
+      {menuSection('About Us')}
+      {menuSection('Catalog')}
     </View>
   </Layout>
 );
