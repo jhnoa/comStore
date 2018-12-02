8e00ed4e4ec5d003403308bcd116e32e17e4a407
AttreaVe <johansiautama@gmail.com>
2018-12-02 18:35:40 +0700

    carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import Layout from '../general/layouts/index';
+import Carousel from 'nuka-carousel';
 
 let menuSection = (name, styles, children) => (
   <TouchableOpacity
@@ -36,6 +37,56 @@ const IndexPage = () => (
         justifyContent: 'center',
         // height: '100%',
         backgroundColor: 'blue',
+      }}
+    >
+      {menuSection(
+        '',
+        {},
+        <Image
+          resizeMode="contain"
+          style={{height: 20, width: 40}}
+          source={require('../assets/picture/google.png')}
+        />,
+      )}
+      {menuSection('', {flex: 1})}
+      {menuSection('Simulation')}
+      {menuSection('About Us')}
+      {menuSection('Catalog')}
+    </View>
+    <Carousel>
+      <img
+        alt="1"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"
+      />
+      <img
+        alt="2"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"
+      />
+      <img
+        alt="3"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"
+      />
+      <img
+        alt="4"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"
+      />
+      <img
+        alt="5"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"
+      />
+      <img
+        alt="6"
+        src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"
+      />
+    </Carousel>
+    <View
+      style={{
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // height: '100%',
+        backgroundColor: 'green',
       }}
     >
       {menuSection(
